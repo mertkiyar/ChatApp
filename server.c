0551b52985acd62307656fddb08360aa4ab1b1b7
mertkiyar <mertkiyar69@gmail.com>
2025-12-14 15:06:20 +0300

    Updated server-client structure to  client-client structure.

--- server.c
+++ server.c
@@ -4,10 +4,14 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <errno.h>
-#define PORT 8080
+
+#define PORT 6378
+
 int main()
 {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    int serverSocket, serverBind, serverListen, clientAddressSize, client1Accept, client2Accept;
+    struct sockaddr_in socketAddress, clientAddress;
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (serverSocket == -1)
     {
@@ -17,13 +21,15 @@ int main()
     {
         printf("Socket created successfuly!\n");
     }
-    struct sockaddr_in socketAdd;
-    socketAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
-    socketAdd.sin_family = AF_INET;
-    socketAdd.sin_port = htons(PORT);
 
-    int serverBind = bind(serverSocket, (struct sockaddr *)&socketAdd, sizeof(socketAdd));
-    if (serverBind == -1)
+    socketAddress.sin_family = AF_INET;
+    socketAddress.sin_port = htons(PORT);       // host to network short
+    socketAddress.sin_addr.s_addr = INADDR_ANY; // inet_addr("127.0.0.1") olsaydı sadece local'den istek kabul ederdi. Şu an her yerden kabul ediyor.
+
+    // BIND
+
+    serverBind = bind(serverSocket, (struct sockaddr *)&socketAddress, sizeof(socketAddress));
+    if (serverBind != 0)
     {
         printf("Error: The bind operation is failed.");
     }
@@ -32,13 +38,42 @@ int main()
         printf("The bind operation is success!\n");
     }
 
-    int serverListen = listen(serverSocket, 1);
-    if (serverListen == -1)
+    // LISTEN
+
+    serverListen = listen(serverSocket, 2); // 2 client dinleniyor
+    if (serverListen != 0)
     {
         printf("Error: The listen operation is failed.");
     }
     else
     {
-        printf("Port %d listening!\n", ntohs(socketAdd.sin_port));
+        printf("Porttt %d listening!\n", ntohs(socketAddress.sin_port));
+        printf("Client 1 waiting...\n");
+    }
+
+    // CLIENT 1
+
+    clientAddressSize = sizeof(clientAddress);
+    client1Accept = accept(serverSocket, (struct sockaddr *)&clientAddress, (socklen_t *)&clientAddressSize);
+    if (client1Accept != 0)
+    {
+        printf("Error: Client 1's request rejected.\n");
+    }
+    else
+    {
+        printf("Client 1's request accepted.\n");
+        printf("Client 2 waiting...\n");
+    }
+
+    // CLIENT 2
+
+    client2Accept = accept(serverSocket, (struct sockaddr *)&clientAddress, (socklen_t *)&clientAddressSize);
+    if (client2Accept != 0)
+    {
+        printf("Client 2's request rejected.\n");
+    }
+    else
+    {
+        printf("Client 2's request accepted.\n");
     }
 }
