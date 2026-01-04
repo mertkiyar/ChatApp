44999f702e4e9ab760d42847004f4ebcbae930e3
mertkiyar <mertkiyar69@gmail.com>
2026-01-04 15:39:28 +0300

    Add file handling functions for reading and writing binary files

---
+++ file.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define RESET "\033[0m"
+
+long getFileSize(const char *fileName)
+{
+    FILE *file = fopen(fileName, "rb"); // rb = read binary
+
+    if (file == 0)
+        return -1;
+
+    fseek(file, 0, SEEK_END); // baştan sona kadar kaç byte olduğunu bul
+    long fileSize = ftell(file);
+    fclose(file);
+    return fileSize;
+}
+
+unsigned char *readFile(const char *fileName, long *fileSize)
+{
+    *fileSize = getFileSize(fileName);
+
+    if (*fileSize == -1)
+        return NULL;
+
+    FILE *file = fopen(fileName, "rb");
+    unsigned char *data = (unsigned char *)malloc(*fileSize); // fileSize boyutuna göre alan ayırır
+
+    if (data == 0)
+    {
+        fclose(file);
+        return NULL;
+    }
+
+    fread(data, 1, *fileSize, file);
+    fclose(file);
+    return data;
+}
+
+void writeFile(const char *fileName, unsigned char *data, int fileSize)
+{
+    FILE *file = fopen(fileName, "wb"); // write binary
+    if (file == 1)
+    {
+        fwrite(data, 1, fileSize, file);
+        fclose(file);
+        printf(GREEN "[+] " RESET "The file %s saved.", fileName);
+    }
+    else
+    {
+        printf(RED "[-] " RESET "The file not saved.");
+    }
+}
